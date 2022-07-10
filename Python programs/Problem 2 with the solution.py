4169971cc6252fb70c24e006798872875950c363
Sajad Soltani <58565246+soltaniSajad1@users.noreply.github.com>
2022-07-10 11:32:59 +0430

    Add files via upload

---
+++ Python programs/Problem 2 with the solution.py
@@ -0,0 +1,16 @@
+# The Area calculation of Circle and Rectangle
+
+Pi = 3.14
+
+RadiusInput = float(input("Please enter teh Radius of Circle: "))
+AreaCircleresult = RadiusInput** 2
+AreaCircleresult = AreaCircleresult * Pi
+print("Your answer is ", AreaCircleresult)
+
+# -----------------------------------------------------------------------------------------
+
+RectangleLengthInput = float(input("Please enter the length of the rectangle: "))
+RectangleWidthInput = float(input("Please enter the width of the rectangle: "))
+
+AreaRectangleResult = RectangleLengthInput * RectangleWidthInput
+print("The Area of Rectangle is ", AreaRectangleResult)
